9a203a37fd605da502656a3f6d4b47eb11b407e9
Charles Theveniau <charlestheveniau@gmail.com>
2025-01-25 18:28:10 +0100

    FE: modified BG on hover to make it ok on darkmode + make only detailed mesurement be collapsible

--- app/components/MesurementItem.vue
+++ app/components/MesurementItem.vue
@@ -1,11 +1,29 @@
 <!-- components/MeasurementItem.vue -->
 <template>
-    <UCollapsible class="hover:bg-gray-50 rounded transition-colors">
-        <div class="measurement-item p-2 " :class="{ 'cursor-pointer': details }">
+    <div v-if="!details">
+        <div class="measurement-item p-2">
             <div class="flex items-center justify-between">
                 <span class="font-medium text-[var(--ui-text)]">{{ label }}</span>
                 <div class="flex items-center gap-2">
-                    <UIcon v-if="details" name="i-heroicons:question-mark-circle" class="size-5 bg-[var(--ui-primary)]" />
+                    
+                    <span class="text-[var(--ui-text)]">{{ value }}cm</span>
+                </div>
+            </div>
+            <div v-if="calculations" class="mt-1 pl-4 text-sm grid gap-1">
+                <div v-for="(val, key) in calculations" :key="key" class="flex justify-between text-[var(--ui-text-dimmed)]">
+                <span>{{ key }}</span>
+                <span>{{ val }}cm</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <UCollapsible v-else class="hover:bg-[var(--ui-bg-accented)] rounded transition-colors">
+        <div class="measurement-item p-2 cursor-pointer">
+            <div class="flex items-center justify-between">
+                <span class="font-medium text-[var(--ui-text)]">{{ label }}</span>
+                <div class="flex items-center gap-2">
+                    <UIcon name="i-heroicons:question-mark-circle" class="size-5 bg-[var(--ui-primary)]" />
                     <span class="text-[var(--ui-text)]">{{ value }}cm</span>
                 </div>
             </div>
